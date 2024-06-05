1205ef07a1418d8106f57c27c04f139af2d92574
CharlieHii <charlie_chaper@hotmail.com>
2024-06-05 12:28:49 +0700

    perf: add log input

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import { useState } from "react";
 function App() {
   const [greeting, setGreeting] = useState("Greeting Message");
   const [inputGreeting, setInpuGreeting] = useState("");
+  const [logGreeting, setLogGreeting] = useState([]);
+  const updateLog = (newGreeting) => {
+    const newLog = [...logGreeting];
+    newLog.push(newGreeting);
+    setLogGreeting(newLog);
+  };
+
+  const deleteLog = (index) => {
+    const newLog = [...logGreeting];
+    newLog.splice(index, 1);
+    setLogGreeting(newLog);
+  };
+
   return (
     <div className="App">
       <div className="greeting-container">{greeting}</div>
@@ -14,11 +27,38 @@ function App() {
           type="text"
           value={inputGreeting}
           onChange={(event) => setInpuGreeting(event.target.value)}
+          className="border border-black bottom-5 px-[20px]"
         />
       </div>
 
-      <div className="buttons" onClick={() => setGreeting(inputGreeting)}>
+      <div
+        className="buttons"
+        onClick={() => {
+          setGreeting(inputGreeting);
+          updateLog(inputGreeting);
+          setInpuGreeting("");
+        }}
+      >
         <button>Update text</button>
+      </div>
+
+      <div className="mt-[20px]">
+        {logGreeting.map((item, index) => (
+          <div
+            className="relative h-[50px] rounded-full bg-slate-300 px-[20px] mt-[5px]"
+            key={index}
+          >
+            <p className="h-full flex justify-center items-center">{item}</p>
+            <button
+              onClick={() => {
+                deleteLog(index);
+              }}
+              className="absolute w-[40px] h-[40px] rounded-full right-[7px] top-[5px] p-2 text-center bg-red-400"
+            >
+              X
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
